f481a70d5b3ebc11173ccdbf37d8901a017b4788
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-09-01 22:20:39 +0530

    feat: build alertService with Kafka consumer from alert-usage
    build emailService and alertService for sending mail to user after reaching energy consumption threshold reached using javaMailSender and MailPit for testing

---
+++ alert-service/src/main/java/com/harsh/alert_service/repository/AlertRepository.java
@@ -0,0 +1,9 @@
+package com.harsh.alert_service.repository;
+
+import com.harsh.alert_service.entity.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+}
